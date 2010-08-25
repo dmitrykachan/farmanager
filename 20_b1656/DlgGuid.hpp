6ed8a1d6a974f896e289e826cc6bf1f47b7a5742
skirda <skirda@ea59307d-ff68-5b67-5a21-acd59c66a581>
2010-08-25 07:19:25 +0000

    tag build 1656

---
+++ 20_b1656/DlgGuid.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+/*
+DlgGuid.hpp
+
+GUID'ы диалогов.
+*/
+/*
+Copyright (c) 2010 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// {8C9EAD29-910F-4b24-A669-EDAFBA6ED964}
+DEFINE_GUID(FindFileId,0x8c9ead29,0x910f,0x4b24,0xa6,0x69,0xed,0xaf,0xba,0x6e,0xd9,0x64);
+// {9FBCB7E1-ACA2-475d-B40D-0F7365B632FF}
+DEFINE_GUID(CopyOverwriteId,0x9fbcb7e1,0xaca2,0x475d,0xb4,0xd,0xf,0x73,0x65,0xb6,0x32,0xff);
+// {1D07CEE2-8F4F-480a-BE93-069B4FF59A2B}
+DEFINE_GUID(FileOpenCreateId,0x1d07cee2,0x8f4f,0x480a,0xbe,0x93,0x6,0x9b,0x4f,0xf5,0x9a,0x2b);
+// {9162F965-78B8-4476-98AC-D699E5B6AFE7}
+DEFINE_GUID(FileSaveAsId,0x9162f965,0x78b8,0x4476,0x98,0xac,0xd6,0x99,0xe5,0xb6,0xaf,0xe7);
+// {FAD00DBE-3FFF-4095-9232-E1CC70C67737}
+DEFINE_GUID(MakeFolderId,0xfad00dbe,0x3fff,0x4095,0x92,0x32,0xe1,0xcc,0x70,0xc6,0x77,0x37);
+// {80695D20-1085-44d6-8061-F3C41AB5569C}
+DEFINE_GUID(FileAttrDlgId,0x80695d20,0x1085,0x44d6,0x80,0x61,0xf3,0xc4,0x1a,0xb5,0x56,0x9c);
---
+++ 20_b1656/vbuild.m4
@@ -0,0 +1,1 @@
+m4_define(BUILD,1656)m4_dnl
