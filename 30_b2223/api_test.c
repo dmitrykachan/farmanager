3473903c472641fa97f6d730b04b57a0939f8952
2useven10 <2useven10@ea59307d-ff68-5b67-5a21-acd59c66a581>
2011-10-16 08:40:02 +0000

    tag build 2223

---
+++ 30_b2223/api_test.c
@@ -0,0 +1,8 @@
+#include "Include/plugin.hpp"
+#include "Include/farcolor.hpp"
+#ifdef __cplusplus
+#if (defined(__GNUC__)) || (defined(_MSC_VER) && _MSC_VER<1600)
+#define nullptr NULL
+#endif
+#include "Include/DlgBuilder.hpp"
+#endif
