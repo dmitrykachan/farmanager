8b84422e5010e64b9c8589bd448bc078a469d26c
drkns <drkns@ea59307d-ff68-5b67-5a21-acd59c66a581>
2013-02-16 10:03:11 +0000

    removed bad tag

--- tags/unicode_far/30_b3199/preservestyle.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-bool PreserveStyleReplaceString(const wchar_t *Source, size_t StrSize, const string& Str, string& ReplaceStr, int& CurPos, int Position, int Case, int WholeWords, const wchar_t *WordDiv, int Reverse, int& SearchLength);
--- tags/unicode_far/30_b3199/vbuild.m4
+++
@@ -1,1 +0,0 @@
-m4_define(BUILD,3199)m4_dnl
