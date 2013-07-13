3225ab0062cb4ffb198750e8b8f2598a8186757d
drkns <drkns@ea59307d-ff68-5b67-5a21-acd59c66a581>
2013-07-13 10:03:25 +0000

    tag build 3517

---
+++ 30_b3517/mix.hpp
@@ -0,0 +1,106 @@
+#pragma once
+
+/*
+mix.hpp
+
+Mix
+*/
+/*
+Copyright © 1996 Eugene Roshal
+Copyright © 2000 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+int ToPercent(unsigned long N1,unsigned long N2);
+int ToPercent64(unsigned __int64 N1,unsigned __int64 N2);
+
+bool FarMkTempEx(string &strDest, const wchar_t *Prefix=nullptr, BOOL WithTempPath=TRUE, const wchar_t *UserTempPath=nullptr);
+
+void PluginPanelItemToFindDataEx(
+    const PluginPanelItem *pSrc,
+    FAR_FIND_DATA *pDest
+);
+
+void FindDataExToPluginPanelItem(
+    const FAR_FIND_DATA *pSrc,
+    PluginPanelItem *pDest
+);
+
+void FreePluginPanelItem(PluginPanelItem& Data);
+
+void FreePluginPanelItemsUserData(HANDLE hPlugin,PluginPanelItem *PanelItem,size_t ItemsNumber);
+
+WINDOWINFO_TYPE ModalType2WType(const int fType);
+
+class SetAutocomplete:NonCopyable
+{
+public:
+	SetAutocomplete(class EditControl* edit, bool NewState = false);
+	SetAutocomplete(class DlgEdit* dedit, bool NewState = false);
+	SetAutocomplete(class CommandLine* cedit, bool NewState = false);
+	~SetAutocomplete();
+
+private:
+	class EditControl* edit;
+	bool OldState;
+};
+
+unsigned long CRC32(unsigned long crc, const void* buffer, size_t len);
+
+struct uuid_hash
+{
+	size_t operator ()(const GUID& Key) const
+	{
+		RPC_STATUS Status;
+		return UuidHash(const_cast<UUID*>(&Key), &Status);
+	}
+};
+
+struct uuid_equal
+{
+	bool operator ()(const GUID& a, const GUID& b) const
+	{
+		// In WinSDK's older than 8.0 operator== for GUIDs declared as int (sic!), This will suppress the warning:
+		return (a == b) != 0;
+	}
+};
+
+struct color_hash
+{
+	size_t operator ()(const FarColor& Key) const
+	{
+		return CRC32(0, &Key, sizeof(Key));
+	}
+};
+
+struct color_equal
+{
+	bool operator ()(const FarColor& a, const FarColor& b) const
+	{
+		return a.ForegroundColor == b.ForegroundColor &&
+			a.BackgroundColor == b.BackgroundColor &&
+			a.Flags == b.Flags;
+	}
+};
