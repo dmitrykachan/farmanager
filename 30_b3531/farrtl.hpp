3faa5433c23e2e775d3f1c00e0cdbeb222070aff
2useven10 <2useven10@ea59307d-ff68-5b67-5a21-acd59c66a581>
2013-07-20 07:56:17 +0000

    tag build 3531

---
+++ 30_b3531/farrtl.hpp
@@ -0,0 +1,48 @@
+
+#pragma once
+
+/*
+farrtl.cpp
+
+Переопределение различных CRT функций
+*/
+
+#ifdef _DEBUG
+#define MEMCHECK
+#endif
+
+#ifdef MEMCHECK
+void* xf_malloc(size_t size, const char* Function, const char* File, int Line);
+void* xf_realloc(void* block, size_t size, const char* Function, const char* File, int Line);
+void* xf_realloc_nomove(void* block, size_t size, const char* Function, const char* File, int Line);
+
+char* DuplicateString(const char* string, const char* Function, const char* File, int Line);
+wchar_t* DuplicateString(const wchar_t* string, const char* Function, const char* File, int Line);
+
+void* operator new(size_t size, const char* Function, const char* File, int Line);
+void* operator new[](size_t size, const char* Function, const char* File, int Line);
+void operator delete(void* block, const char* Function, const char* File, int Line);
+void operator delete[](void* block, const char* Function, const char* File, int Line);
+
+#define xf_malloc(size) xf_malloc(size, __FUNCTION__, __FILE__, __LINE__)
+#define xf_realloc(block, size) xf_realloc(block, size, __FUNCTION__, __FILE__, __LINE__)
+#define xf_realloc_nomove(block, size) xf_realloc_nomove(block, size, __FUNCTION__, __FILE__, __LINE__)
+#define DuplicateString(string) DuplicateString(string, __FUNCTION__, __FILE__, __LINE__)
+#define DuplicateString(string) DuplicateString(string, __FUNCTION__, __FILE__, __LINE__)
+#define new new(__FUNCTION__, __FILE__, __LINE__)
+#else
+void* xf_malloc(size_t size);
+void* xf_realloc_nomove(void* block, size_t size);
+void* xf_realloc(void* block, size_t size);
+char* DuplicateString(const char* string);
+wchar_t* DuplicateString(const wchar_t* string);
+#endif
+
+void PrintMemory();
+
+void  xf_free(void* block);
+char* xstrncpy(char* dest, const char* src, size_t DestSize);
+wchar_t* xwcsncpy(wchar_t* dest, const wchar_t* src, size_t DestSize);
+
+#define ALIGNAS(value, alignment) ((value+(alignment-1))&~(alignment-1))
+#define ALIGN(value) ALIGNAS(value, sizeof(void*))
