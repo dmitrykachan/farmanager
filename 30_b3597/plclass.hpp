8bf0ee8426d1977216eb9d56fbd301fb689b7015
drkns <drkns@ea59307d-ff68-5b67-5a21-acd59c66a581>
2013-08-06 22:45:30 +0000

    tag build 3597

---
+++ 30_b3597/plclass.hpp
@@ -0,0 +1,337 @@
+#pragma once
+
+/*
+Copyright © 1996 Eugene Roshal
+Copyright © 2000 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "bitflags.hpp"
+#include "language.hpp"
+
+class PluginManager;
+class Plugin;
+
+enum EXPORTS_ENUM
+{
+	iGetGlobalInfo,
+	iSetStartupInfo,
+	iOpen,
+	iClosePanel,
+	iGetPluginInfo,
+	iGetOpenPanelInfo,
+	iGetFindData,
+	iFreeFindData,
+	iGetVirtualFindData,
+	iFreeVirtualFindData,
+	iSetDirectory,
+	iGetFiles,
+	iPutFiles,
+	iDeleteFiles,
+	iMakeDirectory,
+	iProcessHostFile,
+	iSetFindList,
+	iConfigure,
+	iExitFAR,
+	iProcessPanelInput,
+	iProcessPanelEvent,
+	iProcessEditorEvent,
+	iCompare,
+	iProcessEditorInput,
+	iProcessViewerEvent,
+	iProcessDialogEvent,
+	iProcessSynchroEvent,
+	iProcessConsoleInput,
+	iAnalyse,
+	iCloseAnalyse,
+	iGetCustomData,
+	iFreeCustomData,
+
+	iOpenFilePlugin,
+	iGetMinFarVersion,
+
+	ExportsCount
+};
+
+
+extern PluginStartupInfo NativeInfo;
+extern FarStandardFunctions NativeFSF;
+
+class GenericPluginModel
+{
+public:
+	typedef void* plugin_instance;
+	typedef void* plugin_module;
+
+	GenericPluginModel(PluginManager* owner);
+	virtual ~GenericPluginModel() {};
+
+	virtual Plugin* CreatePlugin(const string& filename);
+
+	virtual bool IsPlugin(const string& filename) = 0;
+	virtual plugin_module Create(const string& filename) = 0;
+	virtual bool Destroy(plugin_module module) = 0;
+	virtual void InitExports(plugin_instance instance, void** exports) = 0;
+
+	void SaveExportsToCache(class PluginsCacheConfig* cache, unsigned long long id, void* const * exports);
+	void LoadExportsFromCache(class PluginsCacheConfig* cache, unsigned long long id, void** exports);
+
+	template<int> struct ExportType;
+
+	PluginManager* GetOwner() const { return m_owner; }
+	const wchar_t* GetExportName(size_t index) const { return m_ExportsNames[index].UName; }
+
+protected:
+	const struct export_name
+	{
+		const wchar_t* UName;
+		const char* AName;
+	}
+	* m_ExportsNames;
+	PluginManager* m_owner;
+};
+
+class NativePluginModel : public GenericPluginModel
+{
+public:
+	NativePluginModel(PluginManager* owner) : GenericPluginModel(owner) {}
+
+	virtual bool IsPlugin(const string& filename) override;
+	virtual plugin_module Create(const string& filename) override;
+	virtual bool Destroy(plugin_module module) override;
+	virtual void InitExports(plugin_instance instance, void** exports) override;
+
+private:
+	// the rest shouldn't be here, just an optimization for OEM plugins
+	bool IsPlugin2(const void* Module);
+	virtual bool FindExport(const char* ExportName);
+};
+
+class Plugin
+{
+public:
+	Plugin(GenericPluginModel* model, const string& ModuleName);
+	virtual ~Plugin();
+
+	virtual bool GetGlobalInfo(GlobalInfo *Info);
+	virtual bool SetStartupInfo(PluginStartupInfo *Info);
+	virtual HANDLE Open(OpenInfo* Info);
+	virtual void ClosePanel(ClosePanelInfo* Info);
+	virtual bool GetPluginInfo(PluginInfo *pi);
+	virtual void GetOpenPanelInfo(OpenPanelInfo *Info);
+	virtual int GetFindData(GetFindDataInfo* Info);
+	virtual void FreeFindData(FreeFindDataInfo* Info);
+	virtual int GetVirtualFindData(GetVirtualFindDataInfo* Info);
+	virtual void FreeVirtualFindData(FreeFindDataInfo* Info);
+	virtual int SetDirectory(SetDirectoryInfo* Info);
+	virtual int GetFiles(GetFilesInfo* Info);
+	virtual int PutFiles(PutFilesInfo* Info);
+	virtual int DeleteFiles(DeleteFilesInfo* Info);
+	virtual int MakeDirectory(MakeDirectoryInfo* Info);
+	virtual int ProcessHostFile(ProcessHostFileInfo* Info);
+	virtual int SetFindList(SetFindListInfo* Info);
+	virtual int Configure(ConfigureInfo* Info);
+	virtual void ExitFAR(ExitInfo *Info);
+	virtual int ProcessPanelInput(ProcessPanelInputInfo* Info);
+	virtual int ProcessPanelEvent(ProcessPanelEventInfo* Info);
+	virtual int ProcessEditorEvent(ProcessEditorEventInfo* Info);
+	virtual int Compare(CompareInfo* Info);
+	virtual int ProcessEditorInput(ProcessEditorInputInfo* Info);
+	virtual int ProcessViewerEvent(ProcessViewerEventInfo* Info);
+	virtual int ProcessDialogEvent(ProcessDialogEventInfo* Info);
+	virtual int ProcessSynchroEvent(ProcessSynchroEventInfo* Info);
+	virtual int ProcessConsoleInput(ProcessConsoleInputInfo *Info);
+	virtual HANDLE Analyse(AnalyseInfo *Info);
+	virtual void CloseAnalyse(CloseAnalyseInfo* Info);
+
+	virtual int GetCustomData(const wchar_t *FilePath, wchar_t **CustomData);
+	virtual void FreeCustomData(wchar_t *CustomData);
+
+	virtual HANDLE OpenFilePlugin(const wchar_t *Name, const unsigned char *Data, size_t DataSize, int OpMode);
+	virtual bool CheckMinFarVersion();
+
+
+#ifndef NO_WRAPPER
+	virtual bool IsOemPlugin() const { return false; }
+#endif // NO_WRAPPER
+	virtual const string& GetHotkeyName() const { return m_strGuid; }
+
+	virtual bool InitLang(const string& Path) { return PluginLang.Init(Path); }
+	void CloseLang() { PluginLang.Close(); }
+
+	#define HAS_FUNCTION(Name) bool Has##Name() const { return Exports[i##Name] != nullptr; }
+
+	HAS_FUNCTION(GetGlobalInfo)
+	HAS_FUNCTION(Open)
+	HAS_FUNCTION(MakeDirectory)
+	HAS_FUNCTION(DeleteFiles)
+	HAS_FUNCTION(PutFiles)
+	HAS_FUNCTION(GetFiles)
+	HAS_FUNCTION(SetStartupInfo)
+	HAS_FUNCTION(ClosePanel)
+	HAS_FUNCTION(GetPluginInfo)
+	HAS_FUNCTION(GetOpenPanelInfo)
+	HAS_FUNCTION(GetFindData)
+	HAS_FUNCTION(FreeFindData)
+	HAS_FUNCTION(GetVirtualFindData)
+	HAS_FUNCTION(FreeVirtualFindData)
+	HAS_FUNCTION(SetDirectory)
+	HAS_FUNCTION(ProcessHostFile)
+	HAS_FUNCTION(SetFindList)
+	HAS_FUNCTION(Configure)
+	HAS_FUNCTION(ExitFAR)
+	HAS_FUNCTION(ProcessPanelInput)
+	HAS_FUNCTION(ProcessPanelEvent)
+	HAS_FUNCTION(ProcessEditorEvent)
+	HAS_FUNCTION(Compare)
+	HAS_FUNCTION(ProcessEditorInput)
+	HAS_FUNCTION(ProcessViewerEvent)
+	HAS_FUNCTION(ProcessDialogEvent)
+	HAS_FUNCTION(ProcessSynchroEvent)
+	HAS_FUNCTION(ProcessConsoleInput)
+	HAS_FUNCTION(Analyse)
+	HAS_FUNCTION(CloseAnalyse)
+	HAS_FUNCTION(GetCustomData)
+	HAS_FUNCTION(FreeCustomData)
+
+	HAS_FUNCTION(OpenFilePlugin)
+	HAS_FUNCTION(GetMinFarVersion)
+
+	#undef HAS_FUNCTION
+
+	const string& GetModuleName() const { return m_strModuleName; }
+	const string& GetCacheName() const  { return m_strCacheName; }
+	const string& GetTitle() const { return strTitle; }
+	const string& GetDescription() const { return strDescription; }
+	const string& GetAuthor() const { return strAuthor; }
+	const VersionInfo& GetVersion() { return PluginVersion; }
+	const VersionInfo& GetMinFarVersion() { return MinFarVersion; }
+	const string& GetVersionString() { return VersionString; }
+	const GUID& GetGUID() const { return m_Guid; }
+	const wchar_t *GetMsg(LNGID nID) const { return PluginLang.GetMsg(nID); }
+
+	bool CheckWorkFlags(DWORD flags) const { return WorkFlags.Check(flags)==TRUE; }
+	DWORD GetWorkFlags() const { return WorkFlags.Flags(); }
+	DWORD GetFuncFlags() const { return FuncFlags.Flags(); }
+
+	bool Load();
+	int Unload(bool bExitFAR = false);
+	bool LoadData();
+	bool LoadFromCache(const FAR_FIND_DATA &FindData);
+	bool SaveToCache();
+	bool IsPanelPlugin();
+	bool Active() {return Activity != 0;}
+
+protected:
+	struct ExecuteStruct
+	{
+		ExecuteStruct& operator =(intptr_t value) { Result = value; return *this; }
+		ExecuteStruct& operator =(HANDLE value) { Result = reinterpret_cast<intptr_t>(value); return *this; }
+		operator intptr_t() const { return Result; } 
+		operator HANDLE() const { return reinterpret_cast<HANDLE>(Result); }
+
+		EXPORTS_ENUM id;
+
+		intptr_t Default, Result;
+	};
+
+	void ExecuteFunction(ExecuteStruct& es, std::function<void()> f);
+
+	void* Exports[ExportsCount];
+
+	GenericPluginModel *m_model;
+	Language PluginLang;
+	size_t Activity;
+	bool bPendingRemove;
+
+private:
+	virtual void Prologue() {};
+	virtual void Epilogue() {};
+
+	void InitExports();
+	void ClearExports();
+	void SetGuid(const GUID& Guid);
+
+	string strTitle;
+	string strDescription;
+	string strAuthor;
+
+	string m_strModuleName;
+	string m_strCacheName;
+
+	BitFlags WorkFlags;      // рабочие флаги текущего плагина
+	BitFlags FuncFlags;      // битовые маски вызова эксп.функций плагина
+
+	GenericPluginModel::plugin_instance m_Instance;
+
+	VersionInfo MinFarVersion;
+	VersionInfo PluginVersion;
+
+	string VersionString;
+
+	GUID m_Guid;
+	string m_strGuid;
+
+	friend class PluginManager;
+	friend class GenericPluginModel;
+	friend class NativePluginModel;
+};
+
+
+class CustomPluginModel : public GenericPluginModel
+{
+public:
+	CustomPluginModel(PluginManager* owner, const string& filename);
+	~CustomPluginModel();
+
+	bool Success() const { return m_Success; }
+
+	virtual bool IsPlugin(const string& filename) override;
+	virtual plugin_module Create(const string& filename) override;
+	virtual bool Destroy(plugin_module module) override;
+	virtual void InitExports(plugin_instance instance, void** exports) override;
+
+private:
+	template<typename T>
+	inline bool InitImport(T& Address, const char* ProcName)
+	{
+		return (Address = reinterpret_cast<T>(GetProcAddress(m_Module, ProcName))) != nullptr;
+	}
+
+	struct
+	{
+		BOOL (WINAPI *pIsPlugin)(const wchar_t* filename);
+		HANDLE (WINAPI *pCreateInstance)(const wchar_t* filename);
+		FARPROC (WINAPI *pGetFunctionAddress)(HANDLE Instance, const wchar_t* functionname);
+		BOOL (WINAPI *pDestroyInstance)(HANDLE Instance);
+	}
+	Imports;
+	HMODULE m_Module;
+	bool m_Success;
+};
+
+#define EXECUTE_FUNCTION(f) ExecuteFunction(es, [&]{ f; });
+#define FUNCTION(id) reinterpret_cast<id##Prototype>(Exports[id])
+#define WA(string) {L##string, string}
