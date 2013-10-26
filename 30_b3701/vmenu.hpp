4e74618dfadc0390d374ada4a2e812c92ca4dc07
drkns <drkns@ea59307d-ff68-5b67-5a21-acd59c66a581>
2013-10-26 18:12:38 +0000

    tag build 3701

---
+++ 30_b3701/vmenu.hpp
@@ -0,0 +1,405 @@
+#pragma once
+
+/*
+vmenu.hpp
+
+Обычное вертикальное меню
+  а так же:
+    * список в DI_COMBOBOX
+    * список в DI_LISTBOX
+    * ...
+*/
+/*
+Copyright © 1996 Eugene Roshal
+Copyright © 2000 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "modal.hpp"
+#include "bitflags.hpp"
+#include "synchro.hpp"
+
+// Цветовые атрибуты - индексы в массиве цветов
+enum
+{
+	VMenuColorBody                = 0,     // подложка
+	VMenuColorBox                 = 1,     // рамка
+	VMenuColorTitle               = 2,     // заголовок - верхний и нижний
+	VMenuColorText                = 3,     // Текст пункта
+	VMenuColorHilite              = 4,     // HotKey
+	VMenuColorSeparator           = 5,     // separator
+	VMenuColorSelected            = 6,     // Выбранный
+	VMenuColorHSelect             = 7,     // Выбранный - HotKey
+	VMenuColorScrollBar           = 8,     // ScrollBar
+	VMenuColorDisabled            = 9,     // Disabled
+	VMenuColorArrows              =10,     // '<' & '>' обычные
+	VMenuColorArrowsSelect        =11,     // '<' & '>' выбранные
+	VMenuColorArrowsDisabled      =12,     // '<' & '>' Disabled
+	VMenuColorGrayed              =13,     // "серый"
+	VMenuColorSelGrayed           =14,     // выбранный "серый"
+
+	VMENU_COLOR_COUNT,                     // всегда последняя - размерность массива
+};
+
+enum VMENU_FLAGS
+{
+	VMENU_ALWAYSSCROLLBAR      =0x00000100, // всегда показывать скроллбар
+	VMENU_LISTBOX              =0x00000200, // Это список в диалоге
+	VMENU_SHOWNOBOX            =0x00000400, // показать без рамки
+	VMENU_AUTOHIGHLIGHT        =0x00000800, // автоматически выбирать симолы подсветки
+	VMENU_REVERSEHIGHLIGHT     =0x00001000, // ... только с конца
+	VMENU_UPDATEREQUIRED       =0x00002000, // лист необходимо обновить (перерисовать)
+	VMENU_DISABLEDRAWBACKGROUND=0x00004000, // подложку не рисовать
+	VMENU_WRAPMODE             =0x00008000, // зацикленный список (при перемещении)
+	VMENU_SHOWAMPERSAND        =0x00010000, // символ '&' показывать AS IS
+	VMENU_WARNDIALOG           =0x00020000, //
+//	VMENU_NOTCENTER            =0x00040000, // не цитровать
+//	VMENU_LEFTMOST             =0x00080000, // "крайний слева" - нарисовать на 5 позиций вправо от центра (X1 => (ScrX+1)/2+5)
+	VMENU_NOTCHANGE            =0x00100000, //
+	VMENU_LISTHASFOCUS         =0x00200000, // меню является списком в диалоге и имеет фокус
+	VMENU_COMBOBOX             =0x00400000, // меню является комбобоксом и обрабатывается менеджером по-особому.
+	VMENU_MOUSEDOWN            =0x00800000, //
+	VMENU_CHANGECONSOLETITLE   =0x01000000, //
+	VMENU_MOUSEREACTION        =0x02000000, // реагировать на движение мыши? (перемещать позицию при перемещении курсора мыши?)
+	VMENU_DISABLED             =0x04000000, //
+	VMENU_NOMERGEBORDER        =0x08000000, //
+	VMENU_REFILTERREQUIRED     =0x10000000, // перед отрисовкой необходимо обновить фильтр
+	VMENU_LISTSINGLEBOX        =0x20000000, // список, всегда с одинарной рамкой
+};
+
+class Dialog;
+class SaveScreen;
+
+
+struct MenuItemEx:NonCopyable
+{
+	MenuItemEx(const string& Text = L""):
+		strName(Text),
+		Flags(),
+		UserData(),
+		UserDataSize(),
+		ShowPos(),
+		AccelKey(),
+		AmpPos(),
+		Len(),
+		Idx2()
+	{
+	}
+
+	MenuItemEx(MenuItemEx&& rhs):
+		strName(),
+		Flags(),
+		UserData(),
+		UserDataSize(),
+		ShowPos(),
+		AccelKey(),
+		AmpPos(),
+		Len(),
+		Idx2()
+	{
+		*this = std::move(rhs);
+	}
+
+	MenuItemEx& operator=(MenuItemEx&& rhs)
+	{
+		if (this != &rhs)
+		{
+			strName.swap(rhs.strName);
+			std::swap(Flags, rhs.Flags);
+			std::swap(UserData, rhs.UserData);
+			std::swap(UserDataSize, rhs.UserDataSize);
+			std::swap(ShowPos, rhs.ShowPos);
+			std::swap(AccelKey, rhs.AccelKey);
+			std::swap(AmpPos, rhs.AmpPos);
+			std::swap(Len[0], rhs.Len[0]);
+			std::swap(Len[1], rhs.Len[1]);
+			std::swap(Idx2, rhs.Idx2);
+			Annotations.swap(rhs.Annotations);
+		}
+
+		return *this;
+	}
+
+	string strName;
+	UINT64  Flags;                  // Флаги пункта
+	void *UserData;                // Пользовательские данные:
+	size_t UserDataSize;           // Размер пользовательских данных
+	int   ShowPos;
+	DWORD  AccelKey;
+	short AmpPos;                  // Позиция автоназначенной подсветки
+	short Len[2];                  // размеры 2-х частей
+	short Idx2;                    // начало 2-й части
+	std::list<std::pair<int, int>> Annotations;
+
+	UINT64 SetCheck(int Value)
+	{
+		if (Value)
+		{
+			Flags|=LIF_CHECKED;
+			Flags &= ~0xFFFF;
+
+			if (Value!=1) Flags|=Value&0xFFFF;
+		}
+		else
+		{
+			Flags&=~(0xFFFF|LIF_CHECKED);
+		}
+
+		return Flags;
+	}
+
+	UINT64 SetSelect(int Value) { if (Value) Flags|=LIF_SELECTED; else Flags&=~LIF_SELECTED; return Flags;}
+	UINT64 SetDisable(int Value) { if (Value) Flags|=LIF_DISABLE; else Flags&=~LIF_DISABLE; return Flags;}
+};
+
+struct MenuDataEx
+{
+	const wchar_t *Name;
+
+	DWORD Flags;
+	DWORD AccelKey;
+
+	DWORD SetCheck(int Value)
+	{
+		if (Value)
+		{
+			Flags &= ~0xFFFF;
+			Flags|=((Value&0xFFFF)|LIF_CHECKED);
+		}
+		else
+			Flags&=~(0xFFFF|LIF_CHECKED);
+
+		return Flags;
+	}
+
+	DWORD SetSelect(int Value) { if (Value) Flags|=LIF_SELECTED; else Flags&=~LIF_SELECTED; return Flags;}
+	DWORD SetDisable(int Value) { if (Value) Flags|=LIF_DISABLE; else Flags&=~LIF_DISABLE; return Flags;}
+	DWORD SetGrayed(int Value) { if (Value) Flags|=LIF_GRAYED; else Flags&=~LIF_GRAYED; return Flags;}
+};
+
+struct SortItemParam
+{
+	bool Reverse;
+	int Offset;
+};
+
+class ConsoleTitle;
+class Frame;
+
+class VMenu: public Modal
+{
+	private:
+		string strTitle;
+		string strBottomTitle;
+
+		int SelectPos;
+		int TopPos;
+		int MaxHeight;
+		bool WasAutoHeight;
+		int MaxLength;
+		int BoxType;
+		Frame *CurrentFrame;
+		bool PrevCursorVisible;
+		DWORD PrevCursorSize;
+		FARMACROAREA PrevMacroMode;
+
+		// переменная, отвечающая за отображение scrollbar в DI_LISTBOX & DI_COMBOBOX
+		BitFlags VMFlags;
+		BitFlags VMOldFlags;
+
+		Dialog *ParentDialog;         // Для LisBox - родитель в виде диалога
+		size_t DialogItemID;
+		FARWINDOWPROC VMenuProc;      // функция обработки меню
+
+		ConsoleTitle *OldTitle;     // предыдущий заголовок
+
+		CriticalSection CS;
+
+		bool bFilterEnabled;
+		bool bFilterLocked;
+		string strFilter;
+
+		std::vector<MenuItemEx> Items;
+
+		intptr_t ItemHiddenCount;
+		intptr_t ItemSubMenusCount;
+
+		FarColor Colors[VMENU_COLOR_COUNT];
+
+		int MaxLineWidth;
+		bool bRightBtnPressed;
+		GUID MenuId;
+
+	private:
+		virtual void DisplayObject() override;
+		void ShowMenu(bool IsParent=false);
+		void DrawTitles();
+		int GetItemPosition(int Position);
+		static size_t _SetUserData(MenuItemEx *PItem,const void *Data,size_t Size);
+		static void* _GetUserData(MenuItemEx *PItem,void *Data,size_t Size);
+		bool CheckKeyHiOrAcc(DWORD Key,int Type,int Translate);
+		int CheckHighlights(wchar_t Chr,int StartPos=0);
+		wchar_t GetHighlights(const struct MenuItemEx *_item);
+		bool ShiftItemShowPos(int Pos,int Direct);
+		bool ItemCanHaveFocus(UINT64 Flags);
+		bool ItemCanBeEntered(UINT64 Flags);
+		bool ItemIsVisible(UINT64 Flags);
+		void UpdateMaxLengthFromTitles();
+		void UpdateMaxLength(int Length);
+		void UpdateInternalCounters(UINT64 OldFlags, UINT64 NewFlags);
+		bool IsFilterEditKey(int Key);
+		bool ShouldSendKeyToFilter(int Key);
+		//коректировка текущей позиции и флагов SELECTED
+		void UpdateSelectPos();
+
+	public:
+
+		VMenu(const string& Title,
+		      MenuDataEx *Data,
+		      int ItemCount,
+		      int MaxHeight=0,
+		      DWORD Flags=0,
+		      Dialog *ParentDialog=nullptr);
+
+
+		virtual ~VMenu();
+
+		void FastShow() {ShowMenu();}
+		virtual void Show() override;
+		virtual void Hide() override;
+		void ResetCursor();
+
+		void SetTitle(const string& Title);
+		virtual const string& GetTitle(string &strDest) override;
+
+		void SetBottomTitle(const wchar_t *BottomTitle);
+		string &GetBottomTitle(string &strDest);
+		void SetDialogStyle(bool Style) {ChangeFlags(VMENU_WARNDIALOG,Style); SetColors(nullptr);}
+		void SetUpdateRequired(bool SetUpdate) {ChangeFlags(VMENU_UPDATEREQUIRED,SetUpdate);}
+		void SetBoxType(int BoxType);
+
+		void SetFlags(DWORD Flags) { VMFlags.Set(Flags); }
+		void ClearFlags(DWORD Flags) { VMFlags.Clear(Flags); }
+		bool CheckFlags(DWORD Flags) const { return VMFlags.Check(Flags); }
+		DWORD GetFlags() const { return VMFlags.Flags(); }
+		DWORD ChangeFlags(DWORD Flags,bool Status) {return VMFlags.Change(Flags,Status);}
+
+		void AssignHighlights(int Reverse);
+
+		void SetColors(const FarDialogItemColors *ColorsIn=nullptr);
+		void GetColors(FarDialogItemColors *ColorsOut);
+		void SetOneColor(int Index, PaletteColors Color);
+
+		int ProcessFilterKey(int Key);
+		virtual int ProcessKey(int Key) override;
+		virtual int ProcessMouse(const MOUSE_EVENT_RECORD *MouseEvent) override;
+		virtual __int64 VMProcess(int OpCode,void *vParam=nullptr,__int64 iParam=0) override;
+		virtual int ReadInput(INPUT_RECORD *GetReadRec=nullptr) override;
+
+		void DeleteItems();
+		int  DeleteItem(int ID,int Count=1);
+
+		int  AddItem(MenuItemEx& NewItem,int PosAdd=0x7FFFFFFF);
+		int  AddItem(const FarList *NewItem);
+		int  AddItem(const wchar_t *NewStrItem);
+
+		int  InsertItem(const FarListInsert *NewItem);
+		int  UpdateItem(const FarListUpdate *NewItem);
+		int  FindItem(const FarListFind *FindItem);
+		int  FindItem(int StartIndex,const string& Pattern,UINT64 Flags=0);
+		void RestoreFilteredItems();
+		void FilterStringUpdated();
+		void FilterUpdateHeight(bool bShrink=false);
+		void SetFilterEnabled(bool bEnabled) { bFilterEnabled=bEnabled; }
+		void SetFilterLocked(bool bLocked) { bFilterEnabled=bLocked; }
+ 		bool AddToFilter(const wchar_t *str);
+ 		void SetFilterString(const wchar_t *str);
+
+		intptr_t GetItemCount() { return Items.size(); }
+		int  GetShowItemCount() { return static_cast<int>(Items.size())-ItemHiddenCount; }
+		int  GetVisualPos(int Pos);
+		int  VisualPosToReal(int VPos);
+
+		void *GetUserData(void *Data,size_t Size,int Position=-1);
+		size_t GetUserDataSize(int Position=-1);
+		size_t  SetUserData(LPCVOID Data,size_t Size=0,int Position=-1);
+
+		int  GetSelectPos() { return SelectPos; }
+		int  GetSelectPos(FarListPos *ListPos);
+		int  SetSelectPos(const FarListPos *ListPos, int Direct=0);
+		int  SetSelectPos(int Pos, int Direct, bool stop_on_edge=false);
+		int  GetCheck(int Position=-1);
+		void SetCheck(int Check, int Position=-1);
+
+		bool UpdateRequired();
+		void UpdateItemFlags(int Pos, UINT64 NewFlags);
+
+		virtual void ResizeConsole() override;
+
+		struct MenuItemEx *GetItemPtr(int Position=-1);
+
+		void SortItems(bool Reverse = false, int Offset = 0);
+
+		template<class predicate>
+		void SortItems(predicate Pred, bool Reverse = false, int Offset = 0)
+		{
+			CriticalSectionLock Lock(CS);
+
+			SortItemParam Param;
+			Param.Reverse = Reverse;
+			Param.Offset = Offset;
+
+			std::sort(Items.begin(), Items.end(), [&](const MenuItemEx& a, const MenuItemEx& b)->bool
+			{
+				return Pred(a, b, Param);
+			});
+
+			// скорректируем SelectPos
+			UpdateSelectPos();
+
+			SetFlags(VMENU_UPDATEREQUIRED);
+		}
+
+		bool Pack();
+
+		BOOL GetVMenuInfo(struct FarListInfo* Info);
+
+		virtual const wchar_t *GetTypeName() override {return L"[VMenu]";}
+		virtual int GetTypeAndName(string &strType, string &strName) override;
+
+		virtual int GetType() override { return CheckFlags(VMENU_COMBOBOX)?MODALTYPE_COMBOBOX:MODALTYPE_VMENU; }
+
+		void SetMaxHeight(int NewMaxHeight);
+
+		size_t GetVDialogItemID() const {return DialogItemID;}
+		void SetVDialogItemID(size_t NewDialogItemID) {DialogItemID=NewDialogItemID;}
+
+		static FarListItem *MenuItem2FarList(const MenuItemEx *ListItem,FarListItem *Item);
+
+		void SetId(const GUID& Id);
+		const GUID& Id(void);
+
+		static void AddHotkeys(std::vector<string>& Strings, MenuDataEx* Menu, size_t MenuSize);
+};
