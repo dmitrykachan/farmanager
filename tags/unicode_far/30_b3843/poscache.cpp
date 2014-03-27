01e4b35805b6146c52bab77ae809097511fce1ce
vskirdin <vskirdin@ea59307d-ff68-5b67-5a21-acd59c66a581>
2014-03-27 05:35:21 +0000

    tag build 3843

---
+++ tags/unicode_far/30_b3843/poscache.cpp
@@ -0,0 +1,192 @@
+/*
+poscache.cpp
+
+Кэш позиций в файлах для viewer/editor
+*/
+/*
+Copyright © 1996 Eugene Roshal
+Copyright © 2000 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "headers.hpp"
+#pragma hdrstop
+
+#include "poscache.hpp"
+#include "config.hpp"
+#include "configdb.hpp"
+
+void GetFullName(const string& Name, string &strFullName)
+{
+	if (Name[0]==L'<')
+		strFullName = Name;
+	else
+		ConvertNameToFull(Name,strFullName);
+}
+
+void FilePositionCache::CompactHistory()
+{
+	SCOPED_ACTION(auto)(Global->Db->HistoryCfg()->ScopedTransaction());
+	Global->Db->HistoryCfg()->DeleteOldPositions(90,1000);
+}
+
+bool FilePositionCache::AddPosition(const string& Name, const EditorPosCache& poscache)
+{
+	if (!(Global->Opt->EdOpt.SavePos || Global->Opt->EdOpt.SaveShortPos))
+		return false;
+
+	string strFullName;
+	GetFullName(Name,strFullName);
+
+	SCOPED_ACTION(auto)(Global->Db->HistoryCfg()->ScopedTransaction());
+
+	bool ret = false;
+
+	unsigned __int64 id = 0;
+
+	if (Global->Opt->EdOpt.SavePos)
+		id=Global->Db->HistoryCfg()->SetEditorPos(strFullName, poscache.cur.Line, poscache.cur.LinePos, poscache.cur.ScreenLine, poscache.cur.LeftPos, poscache.CodePage);
+	else if (Global->Opt->EdOpt.SaveShortPos)
+		id=Global->Db->HistoryCfg()->SetEditorPos(strFullName, 0, 0, 0, 0, 0);
+
+	if (id)
+	{
+		if (Global->Opt->EdOpt.SaveShortPos)
+		{
+			for_each_cnt(CONST_RANGE(poscache.bm, i, size_t index)
+			{
+				if (i.Line != POS_NONE)
+					Global->Db->HistoryCfg()->SetEditorBookmark(id, index, i.Line, i.LinePos, i.ScreenLine, i.LeftPos);
+			});
+		}
+		ret = true;
+	}
+
+	return ret;
+}
+
+bool FilePositionCache::GetPosition(const string& Name, EditorPosCache& poscache)
+{
+	poscache.Clear();
+
+	string strFullName;
+	GetFullName(Name,strFullName);
+
+	unsigned __int64 id = 0;
+
+	if (Global->Opt->EdOpt.SavePos || Global->Opt->EdOpt.SaveShortPos)
+		id = Global->Db->HistoryCfg()->GetEditorPos(strFullName, &poscache.cur.Line, &poscache.cur.LinePos, &poscache.cur.ScreenLine, &poscache.cur.LeftPos, &poscache.CodePage);
+
+	if (!Global->Opt->EdOpt.SavePos)
+	{
+		poscache.Clear();
+	}
+
+	if (id)
+	{
+		if (!Global->Opt->EdOpt.SaveShortPos)
+			return true;
+
+		for_each_cnt(RANGE(poscache.bm, i, size_t index)
+		{
+			Global->Db->HistoryCfg()->GetEditorBookmark(id, index, &i.Line, &i.LinePos, &i.ScreenLine, &i.LeftPos);
+		});
+
+		return true;
+	}
+
+	return false;
+}
+
+bool FilePositionCache::AddPosition(const string& Name, const ViewerPosCache& poscache)
+{
+	if (!(Global->Opt->ViOpt.SavePos || Global->Opt->ViOpt.SaveCodepage || Global->Opt->ViOpt.SaveWrapMode || Global->Opt->ViOpt.SaveShortPos))
+		return false;
+
+	string strFullName;
+	GetFullName(Name,strFullName);
+
+	SCOPED_ACTION(auto)(Global->Db->HistoryCfg()->ScopedTransaction());
+
+	bool ret = false;
+	unsigned __int64 id = 0;
+
+	if (Global->Opt->ViOpt.SavePos || Global->Opt->ViOpt.SaveCodepage || Global->Opt->ViOpt.SaveWrapMode)
+		id=Global->Db->HistoryCfg()->SetViewerPos(strFullName,
+				Global->Opt->ViOpt.SavePos?poscache.cur.FilePos:0,
+				Global->Opt->ViOpt.SavePos?poscache.cur.LeftPos:0,
+				Global->Opt->ViOpt.SaveWrapMode?poscache.Hex_Wrap:0,
+				Global->Opt->ViOpt.SaveCodepage?poscache.CodePage:0);
+	else if (Global->Opt->ViOpt.SaveShortPos)
+		id=Global->Db->HistoryCfg()->SetViewerPos(strFullName, 0, 0, 0, 0);
+
+	if (id)
+	{
+		if (Global->Opt->ViOpt.SaveShortPos)
+		{
+			for_each_cnt(CONST_RANGE(poscache.bm, i, size_t index)
+			{
+				if (i.FilePos != POS_NONE)
+					Global->Db->HistoryCfg()->SetViewerBookmark(id, index, i.FilePos, i.LeftPos);
+			});
+		}
+		ret = true;
+	}
+
+	return ret;
+}
+
+bool FilePositionCache::GetPosition(const string& Name, ViewerPosCache& poscache)
+{
+	poscache.Clear();
+
+	string strFullName;
+	GetFullName(Name,strFullName);
+
+	unsigned __int64 id = 0;
+
+	if (Global->Opt->ViOpt.SavePos || Global->Opt->ViOpt.SaveCodepage || Global->Opt->ViOpt.SaveWrapMode || Global->Opt->ViOpt.SaveShortPos)
+		id = Global->Db->HistoryCfg()->GetViewerPos(strFullName, &poscache.cur.FilePos, &poscache.cur.LeftPos, &poscache.Hex_Wrap, &poscache.CodePage);
+
+	if (!Global->Opt->ViOpt.SavePos && !Global->Opt->ViOpt.SaveCodepage && !Global->Opt->ViOpt.SaveWrapMode)
+	{
+		poscache.Clear();
+	}
+
+	if (id)
+	{
+		if (!Global->Opt->ViOpt.SaveShortPos)
+			return true;
+
+		for_each_cnt(RANGE(poscache.bm, i, size_t index)
+		{
+			Global->Db->HistoryCfg()->GetViewerBookmark(id, index, &i.FilePos, &i.LeftPos);
+		});
+
+		return true;
+	}
+
+	return false;
+}
---
+++ tags/unicode_far/30_b3843/vbuild.m4
@@ -0,0 +1,1 @@
+m4_define(BUILD,3843)m4_dnl
