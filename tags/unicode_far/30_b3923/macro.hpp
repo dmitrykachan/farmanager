01a10e90dc8e13d60a860e5496a0262f05ec129a
solomuz0 <solomuz0@ea59307d-ff68-5b67-5a21-acd59c66a581>
2014-05-26 15:47:17 +0000

    tag build 3923

---
+++ tags/unicode_far/30_b3923/macro.hpp
@@ -0,0 +1,163 @@
+#pragma once
+
+/*
+macro.hpp
+
+Макросы
+*/
+/*
+Copyright © 1996 Eugene Roshal
+Copyright © 2000 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "tvar.hpp"
+#include "noncopyable.hpp"
+
+// Macro Const
+enum {
+	constMsX          = 0,
+	constMsY          = 1,
+	constMsButton     = 2,
+	constMsCtrlState  = 3,
+	constMsEventFlags = 4,
+	constMsLastCtrlState = 5,
+	constMsLAST       = 6,
+};
+
+enum MACRODISABLEONLOAD
+{
+	MDOL_ALL            = 0x80000000, // дисаблим все макросы при загрузке
+	MDOL_AUTOSTART      = 0x00000001, // дисаблим автостартующие макросы
+};
+
+typedef unsigned __int64 MACROFLAGS_MFLAGS;
+static const MACROFLAGS_MFLAGS
+	// public flags, read from/saved to config
+	MFLAGS_PUBLIC_MASK             =0x000000000FFFFFFF,
+	MFLAGS_ENABLEOUTPUT            =0x0000000000000001, // не подавлять обновление экрана во время выполнения макроса
+	MFLAGS_NOSENDKEYSTOPLUGINS     =0x0000000000000002, // НЕ передавать плагинам клавиши во время записи/воспроизведения макроса
+	MFLAGS_RUNAFTERFARSTART        =0x0000000000000008, // этот макрос запускается при старте ФАРа
+	MFLAGS_EMPTYCOMMANDLINE        =0x0000000000000010, // запускать, если командная линия пуста
+	MFLAGS_NOTEMPTYCOMMANDLINE     =0x0000000000000020, // запускать, если командная линия не пуста
+	MFLAGS_EDITSELECTION           =0x0000000000000040, // запускать, если есть выделение в редакторе
+	MFLAGS_EDITNOSELECTION         =0x0000000000000080, // запускать, если есть нет выделения в редакторе
+	MFLAGS_SELECTION               =0x0000000000000100, // активная:  запускать, если есть выделение
+	MFLAGS_PSELECTION              =0x0000000000000200, // пассивная: запускать, если есть выделение
+	MFLAGS_NOSELECTION             =0x0000000000000400, // активная:  запускать, если есть нет выделения
+	MFLAGS_PNOSELECTION            =0x0000000000000800, // пассивная: запускать, если есть нет выделения
+	MFLAGS_NOFILEPANELS            =0x0000000000001000, // активная:  запускать, если это плагиновая панель
+	MFLAGS_PNOFILEPANELS           =0x0000000000002000, // пассивная: запускать, если это плагиновая панель
+	MFLAGS_NOPLUGINPANELS          =0x0000000000004000, // активная:  запускать, если это файловая панель
+	MFLAGS_PNOPLUGINPANELS         =0x0000000000008000, // пассивная: запускать, если это файловая панель
+	MFLAGS_NOFOLDERS               =0x0000000000010000, // активная:  запускать, если текущий объект "файл"
+	MFLAGS_PNOFOLDERS              =0x0000000000020000, // пассивная: запускать, если текущий объект "файл"
+	MFLAGS_NOFILES                 =0x0000000000040000, // активная:  запускать, если текущий объект "папка"
+	MFLAGS_PNOFILES                =0x0000000000080000, // пассивная: запускать, если текущий объект "папка"
+
+	// private flags, for runtime purposes only
+	MFLAGS_PRIVATE_MASK            =0xFFFFFFFFF0000000,
+	MFLAGS_POSTFROMPLUGIN          =0x0000000010000000; // последовательность пришла от АПИ
+
+
+// коды возврата для KeyMacro::GetCurRecord()
+enum MACRORECORDANDEXECUTETYPE
+{
+	MACROMODE_NOMACRO          =0,  // не в режиме макро
+	MACROMODE_EXECUTING        =1,  // исполнение: без передачи плагину пимп
+	MACROMODE_EXECUTING_COMMON =2,  // исполнение: с передачей плагину пимп
+	MACROMODE_RECORDING        =3,  // запись: без передачи плагину пимп
+	MACROMODE_RECORDING_COMMON =4,  // запись: с передачей плагину пимп
+};
+
+struct MacroPanelSelect
+{
+	__int64 Index;
+	TVar    *Item;
+	int     Action;
+	DWORD   ActionFlags;
+	int     Mode;
+};
+
+class Dialog;
+
+class KeyMacro: NonCopyable
+{
+public:
+	KeyMacro();
+
+	static bool AddMacro(const GUID& PluginId,const MacroAddMacro* Data);
+	static bool DelMacro(const GUID& PluginId,void* Id);
+	static bool ExecuteString(MacroExecuteString *Data);
+	static bool GetMacroKeyInfo(const string& strMode,int Pos,string &strKeyName,string &strDescription);
+	static int  IsDisableOutput();
+	static int  IsExecuting();
+	static bool IsHistoryDisable(int TypeHistory);
+	static bool MacroExists(int Key, FARMACROAREA CheckMode, bool UseCommon);
+	static void RunStartMacro();
+	static bool Save(bool always);
+	static void SendDropProcess();
+	static void SetMacroConst(int ConstIndex, __int64 Value);
+
+	intptr_t CallFar(intptr_t OpCode, FarMacroCall* Data);
+	void CallPluginSynchro(MacroPluginReturn *Params, FarMacroCall **Target, int *Boolean);
+	bool CheckWaitKeyFunc() const;
+	int  GetCurRecord() const;
+	int  GetKey();
+	void GetMacroParseError(DWORD* ErrCode, COORD* ErrPos, string *ErrSrc) const;
+	FARMACROAREA GetMode() const { return m_Mode; }
+	const wchar_t* GetStringToPrint() const { return m_StringToPrint; }
+	int  IsRecording() const { return m_Recording; }
+	bool Load(bool InitedRAM=true,bool LoadAll=true);
+	bool ParseMacroString(const wchar_t* Sequence,FARKEYMACROFLAGS Flags,bool skipFile);
+	int  PeekKey() const;
+	bool PostNewMacro(const wchar_t* Sequence,FARKEYMACROFLAGS Flags,DWORD AKey=0);
+	int  ProcessEvent(const FAR_INPUT_RECORD *Rec);
+	void SetMode(FARMACROAREA Mode) { m_Mode=Mode; }
+	void SuspendMacros(bool Suspend) { Suspend ? ++m_InternalInput : --m_InternalInput; }
+
+private:
+	intptr_t AssignMacroDlgProc(Dialog* Dlg,intptr_t Msg,intptr_t Param1,void* Param2);
+	int  AssignMacroKey(DWORD& MacroKey,UINT64& Flags);
+	void CallPlugin(MacroPluginReturn *mpr, FarMacroValue *fmv, bool CallPluginRules);
+	int  GetMacroSettings(int Key,UINT64 &Flags,const wchar_t *Src=nullptr,const wchar_t *Descr=nullptr);
+	intptr_t ParamMacroDlgProc(Dialog* Dlg,intptr_t Msg,intptr_t Param1,void* Param2);
+	void RestoreMacroChar() const;
+
+	FARMACROAREA m_Mode;
+	FARMACROAREA m_RecMode;
+	FARMACROAREA m_StartMode;
+	MACRORECORDANDEXECUTETYPE m_Recording;
+	string m_RecCode;
+	string m_RecDescription;
+	string m_LastErrorStr;
+	int m_LastErrorLine;
+	int m_InternalInput;
+	int m_DisableNested;
+	int m_WaitKey;
+	const wchar_t* m_StringToPrint;
+};
+
+inline bool IsMenuArea(int Area){return Area==MACROAREA_MAINMENU || Area==MACROAREA_MENU || Area==MACROAREA_DISKS || Area==MACROAREA_USERMENU || Area==MACROAREA_SHELLAUTOCOMPLETION || Area==MACROAREA_DIALOGAUTOCOMPLETION;}
