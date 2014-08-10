5fff4f6e2997640d1251bf385ac02153061c5451
alabuzhev <alabuzhev@ea59307d-ff68-5b67-5a21-acd59c66a581>
2014-08-10 08:22:49 +0000

    tag build 4041

---
+++ tags/unicode_far/30_b4041/scrbuf.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+/*
+scrbuf.hpp
+
+Буферизация вывода на экран, весь вывод идет через этот буфер
+*/
+/*
+Copyright © 1996 Eugene Roshal
+Copyright © 2000 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "bitflags.hpp"
+#include "synchro.hpp"
+#include "matrix.hpp"
+
+class ScreenBuf: NonCopyable
+{
+public:
+	ScreenBuf();
+
+	void AllocBuf(size_t rows, size_t cols);
+	void Lock();
+	void Unlock();
+	int  GetLockCount() const {return LockCount;}
+	void SetLockCount(int Count);
+	void ResetLockCount() {LockCount=0;}
+	void ResetShadow();
+	void MoveCursor(int X,int Y);
+	void GetCursorPos(SHORT& X, SHORT& Y) const;
+	void SetCursorType(bool Visible, DWORD Size);
+	void GetCursorType(bool& Visible, DWORD& Size) const;
+
+	void FillBuf();
+	void Read(int X1,int Y1,int X2,int Y2,FAR_CHAR_INFO *Text,size_t Size);
+	void Write(int X,int Y,const FAR_CHAR_INFO *Text, size_t Size);
+	void RestoreMacroChar();
+	void RestoreElevationChar();
+
+	void ApplyShadow(int X1,int Y1,int X2,int Y2);
+	void ApplyColor(int X1,int Y1,int X2,int Y2,const FarColor& Color, bool PreserveExFlags = false);
+	void ApplyColor(int X1,int Y1,int X2,int Y2,const FarColor& Color,const FarColor& ExceptColor, bool ForceExFlags = false);
+	void FillRect(int X1,int Y1,int X2,int Y2,WCHAR Ch,const FarColor& Color);
+
+	void Scroll(int);
+	void Flush(bool SuppressIndicators = false);
+
+private:
+	CriticalSection CS;
+	FAR_CHAR_INFO MacroChar;
+	FAR_CHAR_INFO ElevationChar;
+	matrix<FAR_CHAR_INFO> Buf;
+	matrix<FAR_CHAR_INFO> Shadow;
+	BitFlags SBFlags;
+	int LockCount;
+	DWORD CurSize;
+	SHORT CurX, CurY;
+	bool MacroCharUsed;
+	bool ElevationCharUsed;
+	bool CurVisible;
+};
