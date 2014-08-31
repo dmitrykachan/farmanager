a9b814e45b9ec242f703ace979c2e040b2b401b7
alabuzhev <alabuzhev@ea59307d-ff68-5b67-5a21-acd59c66a581>
2014-08-31 14:30:20 +0000

    tag build 4073

---
+++ 30_b4073/grabber.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+/*
+grabber.hpp
+
+Screen grabber
+*/
+/*
+Copyright © 1996 Eugene Roshal
+Copyright © 2000 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "modal.hpp"
+#include "macro.hpp"
+
+class Grabber:Modal
+{
+public:
+	Grabber();
+	virtual ~Grabber();
+
+	virtual int GetType() const override { return windowtype_grabber; }
+	virtual int GetTypeAndName(string &, string &) override { return windowtype_grabber; }
+	virtual void ResizeConsole(void) override;
+
+private:
+	virtual void DisplayObject() override;
+	virtual int ProcessKey(const Manager::Key& Key) override;
+	virtual int ProcessMouse(const MOUSE_EVENT_RECORD *MouseEvent) override;
+	virtual string GetTitle() const override { return string(); }
+
+	void CopyGrabbedArea(bool Append, bool VerticalBlock);
+	void Reset();
+
+	struct GrabberArea
+	{
+		SHORT X1, Y1, X2, Y2;
+		SHORT CurX, CurY;
+	};
+
+	GrabberArea PrevArea;
+	GrabberArea GArea;
+	bool ResetArea;
+	bool m_VerticalBlock;
+};
+
+bool RunGraber();
