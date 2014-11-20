6d12de0e08668983fa8f8c52f3fca20e14f99c0b
oo.zg.oo <oo.zg.oo@ea59307d-ff68-5b67-5a21-acd59c66a581>
2014-11-20 11:23:05 +0000

    tag build 4200

---
+++ 30_b4200/TaskBar.cpp
@@ -0,0 +1,140 @@
+/*
+TaskBar.cpp
+
+Windows 7 taskbar support
+*/
+/*
+Copyright © 2009 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "headers.hpp"
+#pragma hdrstop
+
+#include "TaskBar.hpp"
+#include "console.hpp"
+
+taskbar& Taskbar()
+{
+	static taskbar tb;
+	return tb;
+}
+
+taskbar::taskbar():
+	State(TBPF_NOPROGRESS),
+	pTaskbarList(nullptr)
+{
+	HRESULT hRes=CoInitializeEx(nullptr,COINIT_APARTMENTTHREADED);
+
+	switch (hRes)
+	{
+		case S_OK:
+		case S_FALSE:
+			CoInited=true;
+		case RPC_E_CHANGED_MODE:
+#ifdef _MSC_VER
+			assert(__uuidof(*pTaskbarList) == IID_ITaskbarList3);
+#endif
+			if (CoCreateInstance(CLSID_TaskbarList, nullptr, CLSCTX_INPROC_SERVER, IID_ITaskbarList3, IID_PPV_ARGS_Helper(&pTaskbarList)) != S_OK)
+				pTaskbarList = nullptr;
+			break;
+		default:
+			CoInited=false;
+			break;
+	}
+}
+
+taskbar::~taskbar()
+{
+	if (pTaskbarList)
+	{
+		pTaskbarList->Release();
+	}
+
+	if (CoInited)
+	{
+		CoUninitialize();
+	}
+}
+
+void taskbar::SetProgressState(TBPFLAG tbpFlags)
+{
+	if (pTaskbarList)
+	{
+		State=tbpFlags;
+		pTaskbarList->SetProgressState(Console().GetWindow(),tbpFlags);
+	}
+}
+
+void taskbar::SetProgressValue(UINT64 Completed, UINT64 Total)
+{
+	if (pTaskbarList)
+	{
+		State=TBPF_NORMAL;
+		pTaskbarList->SetProgressValue(Console().GetWindow(),Completed,Total);
+	}
+}
+
+TBPFLAG taskbar::GetProgressState()
+{
+	return State;
+}
+
+void taskbar::Flash()
+{
+	WINDOWINFO WI={sizeof(WI)};
+
+	if (GetWindowInfo(Console().GetWindow(),&WI))
+	{
+		if (WI.dwWindowStatus!=WS_ACTIVECAPTION)
+		{
+			FLASHWINFO FWI={sizeof(FWI),Console().GetWindow(),FLASHW_ALL|FLASHW_TIMERNOFG,5,0};
+			FlashWindowEx(&FWI);
+		}
+	}
+}
+
+
+
+IndeterminateTaskBar::IndeterminateTaskBar(bool EndFlash):
+	EndFlash(EndFlash)
+{
+	if (Taskbar().GetProgressState()!=TBPF_INDETERMINATE)
+	{
+		Taskbar().SetProgressState(TBPF_INDETERMINATE);
+	}
+}
+
+IndeterminateTaskBar::~IndeterminateTaskBar()
+{
+	if (Taskbar().GetProgressState()!=TBPF_NOPROGRESS)
+	{
+		Taskbar().SetProgressState(TBPF_NOPROGRESS);
+	}
+	if(EndFlash)
+	{
+		Taskbar().Flash();
+	}
+}
