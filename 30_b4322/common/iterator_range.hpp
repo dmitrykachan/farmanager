cbc89700bcd68a8c66409516f235821cd92d5651
oo.zg.oo <oo.zg.oo@ea59307d-ff68-5b67-5a21-acd59c66a581>
2015-03-21 14:50:14 +0000

    tag build 4322

---
+++ 30_b4322/common/iterator_range.hpp
@@ -0,0 +1,114 @@
+#pragma once
+
+/*
+Copyright © 2014 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+template<class iterator_type>
+class range
+{
+public:
+	typedef iterator_type iterator;
+	typedef std::reverse_iterator<iterator> reverse_iterator;
+	typedef typename std::iterator_traits<iterator>::reference reference;
+	typedef typename std::iterator_traits<iterator>::pointer pointer;
+	typedef typename std::iterator_traits<iterator>::value_type value_type;
+
+	range(iterator i_begin, iterator i_end):
+		m_begin(i_begin),
+		m_end(i_end)
+	{}
+
+	iterator begin() const { return m_begin; }
+	iterator end() const { return m_end; }
+	reverse_iterator rbegin() const { return reverse_iterator(m_end); }
+	reverse_iterator rend() const { return reverse_iterator(m_begin); }
+
+	reference operator[](size_t n) { return *(m_begin + n); }
+	const reference operator[](size_t n) const { return *(m_begin + n); }
+
+	reference front() { return *m_begin; }
+	reference back() { return *std::prev(m_end); }
+
+	pointer data() { return &*m_begin; }
+	pointer data() const { return &*m_begin; }
+
+	bool empty() const { return m_begin == m_end; }
+	size_t size() const { return m_end - m_begin; }
+
+private:
+	iterator m_begin;
+	iterator m_end;
+};
+
+template<class iterator_type>
+inline range<iterator_type> make_range(iterator_type i_begin, iterator_type i_end)
+{
+	return range<iterator_type>(i_begin, i_end);
+}
+
+template<class T>
+class i_iterator: public std::iterator < std::random_access_iterator_tag, T>
+{
+public:
+	typedef const T value_type;
+
+	i_iterator(const T& value): m_value(value) {}
+	i_iterator(const i_iterator& rhs): m_value(rhs.m_value) {}
+	i_iterator& operator=(const i_iterator& rhs) { m_value = rhs.m_value; return *this; }
+	const value_type* operator->() const { return &m_value; }
+	const value_type& operator*() const { return m_value; }
+	i_iterator& operator++() { ++m_value; return *this; }
+	i_iterator& operator--() { --m_value; return *this; }
+	i_iterator& operator+=(size_t n) { m_value += n; return *this; }
+	i_iterator& operator-=(size_t n) { m_value -= n; return *this; }
+	i_iterator operator++(int) { return m_value++; }
+	i_iterator operator--(int) { return m_value--; }
+	i_iterator operator+(size_t n) const { return m_value + n; }
+	i_iterator operator-(size_t n) const { return m_value - n; }
+	ptrdiff_t operator-(const i_iterator& rhs) const { return m_value - rhs.m_value; }
+	bool operator==(const i_iterator& rhs) const { return m_value == rhs.m_value; }
+	bool operator<(const i_iterator& rhs) const { return m_value < rhs.m_value; }
+	bool operator<=(const i_iterator& rhs) const { return m_value <= rhs.m_value; }
+	bool operator!=(const i_iterator& rhs) const { return !(*this == rhs); }
+	bool operator>=(const i_iterator& rhs) const { return !(*this < rhs); }
+	bool operator>(const i_iterator& rhs) const { return !(*this <= rhs); }
+
+private:
+	T m_value;
+};
+
+template<class T>
+inline range<i_iterator<T>> make_irange(T i_begin, T i_end)
+{
+	return range<i_iterator<T>>(i_begin, i_end);
+}
+
+template<class T>
+inline range<i_iterator<T>> make_irange(T i_end)
+{
+	return range<i_iterator<T>>(0, i_end);
+}
---
+++ 30_b4322/usermenu.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+/*
+usermenu.hpp
+
+User menu и есть
+*/
+/*
+Copyright © 1996 Eugene Roshal
+Copyright © 2000 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+ENUM(MENUMODE);
+
+class UserMenu: noncopyable
+{
+	struct UserMenuItem;
+
+public:
+	UserMenu(bool MenuType); //	true - выбор типа меню (основное или локальное), false - зависит от наличия FarMenu.Ini в текущем каталоге
+	UserMenu(const string& MenuFileName);
+	~UserMenu();
+
+	typedef std::list<UserMenuItem> menu_container;
+
+private:
+	void ProcessUserMenu(bool MenuType, const string& MenuFileName);
+	bool DeleteMenuRecord(menu_container& Menu, const menu_container::iterator& MenuItem);
+	bool EditMenu(menu_container& Menu, menu_container::iterator* MenuItem, bool Create);
+	int ProcessSingleMenu(menu_container& Menu, int MenuPos, menu_container& MenuRoot, const string& MenuFileName, const string& Title);
+	void SaveMenu(const string& MenuFileName);
+	intptr_t EditMenuDlgProc(Dialog* Dlg,intptr_t Msg,intptr_t Param1,void* Param2);
+
+	MENUMODE m_MenuMode;
+	bool m_MenuModified;
+	bool m_ItemChanged;
+	menu_container m_Menu;
+};
